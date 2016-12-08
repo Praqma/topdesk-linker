6b1ff7fe3af21f6a53e06174a90ee4229dbfceaf
Andrius Ordojan <andrius.or@gmail.com>
2016-12-08 15:18:45 +0100

    Added variable issue id

--- src/main/java/com/praqma/plugins/atlassian/jira/jira/webwork/UpdateTopDeskAction.java
+++ src/main/java/com/praqma/plugins/atlassian/jira/jira/webwork/UpdateTopDeskAction.java
@@ -8,9 +8,26 @@ public class UpdateTopDeskAction extends JiraWebActionSupport {
 
     private static final Logger log = LoggerFactory.getLogger(UpdateTopDeskAction.class);
     
+    String issue;
+
+    public String getIssue() {
+        return issue;
+    }
+
+    public void setIssue(String issue) {
+        this.issue = issue;
+    }
+    
     @Override
     public String execute() throws Exception {
-        log.warn("IT WORKS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        log.warn("IT WORKZZZZZZZZZZZ!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        if (issue == null) {
+            log.warn("it's fucking null");
+        } else {
+            log.warn("TADA!!");
+            log.warn(issue);
+        }
+        
         return super.execute(); //returns SUCCESS
     }
 }
